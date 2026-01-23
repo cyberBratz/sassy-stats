47461bc4dd6cca7ecb585e219ed2c74fc104cd82
ValeriaMejiaV <vameve@fp.insjoaquimmir.cat>
2026-01-24 00:03:13 +0100

    intentado el perfil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,35 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './script'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
-        <h1 class="text-3xl font-bold underline">
-        Hello world!
+        
+      <h1 className='underline'>
+        Display your Spotify profile data
       </h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <section id="profile">
+      <h2>Logged in as <span id="displayName"></span></h2>
+      <span id="avatar"></span>
+      <ul>
+          <li>User ID: <span id="id"></span></li>
+          <li>Email: <span id="email"></span></li>
+          <li>Spotify URI: <a id="uri" href="#"></a></li>
+          <li>Link: <a id="url" href="#"></a></li>
+          <li>Profile Image: <span id="imgUrl"></span></li>
+      </ul>
+      </section>
     </>
   )
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+
+     plugins: [react(), tailwindcss()],
+      server: {
+        host:true,
+        port: 5173,
+        hmr: {
+          clientPort: 5173
+        }
+       
+      },
+      define: {
+          'process.env.CLIENT_ID': JSON.stringify(env.CLIENT_ID),
+          'process.env.CLIENT_SECRET': JSON.stringify(env.CLIENT_SECRET),
+      },
+  };
+});
